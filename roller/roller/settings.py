95d65c390533d8ad81350bd8b383ecde16568046
William Walter <william.walter@gmail.com>
2017-03-26 23:03:05 -0400

    adding bcrypt requirements, adding hashers to settings

--- roller/roller/settings.py
+++ roller/roller/settings.py
@@ -92,6 +92,12 @@ DATABASES = {
 }
 
 
+PASSWORD_HASHER = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    ]
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
