3e3a70645195df5f0c016d95405cbf57b5684a3a
William Walter <william.walter@gmail.com>
2017-04-04 23:50:13 -0400

    character and campaign models, made the migration to see what it looks like, haven't run either on my local

--- roller/player/models.py
+++ roller/player/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from userauth.models import User
 
-# Create your models here.
+class Character(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    experience = models.PositiveIntegerField(default=0)
+    height = models.PositiveSmallIntegerField(default=60)
+    weight = models.PositiveSmallIntegerField(default=60)
+    age = models.PositiveSmallIntegerField(default=20)
+    level = models.PositiveSmallIntegerField(default=1)
+
+class Campaign(models.Model):
+    master = models.ForeignKey(User, on_delete=models.CASCADE)
+    users = models.ManyToManyField(Character)
--- roller/roller/settings.py
+++ roller/roller/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'player',
     'userauth',
     'django.contrib.admin',
     'django.contrib.auth',
