a4b7b0227301558dcb6367e6dda6579a85c140ec
William Walter <william.walter@gmail.com>
2017-04-04 22:57:17 -0400

    playing with templates

--- roller/userauth/urls.py
+++ roller/userauth/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
+        url(r'^register', views.register, name='register'),
+        url(r'^confirmation', views.confirmation, name='confirmation'),
         ]
--- roller/userauth/views.py
+++ roller/userauth/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello, world")
+
+def register(request):
+    return render(request,'userauth/register.html')
+
+def confirmation(request):
+    return HttpResponse("First: {} Last: {} Email: {}".format(request.POST['firstname'], request.POST['lastname'], request.POST['email']))
+
